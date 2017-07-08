ae6e02e4a61724b0eeb9a4de3df9eb8f0cccfe48
niinyarko <niinyarko1@gmail.com>
2017-07-08 09:56:54 +0000

    setup publishComposite for siteConfig.

--- app/imports/api/site-config/server/publications.js
+++ app/imports/api/site-config/server/publications.js
@@ -1,7 +1,32 @@
-import { Meteor } from 'meteor/meteor';
+import { publishComposite } from 'meteor/reywood:publish-composite';
 import SiteConfig from '../site-config.js';
+import FrontPageTemplates from '../../front-page-templates/front-page-templates.js';
+import FeaturedGroups from '../../featured-groups/featured-groups.js';
+import FeaturedItems from '../../featured-items/featured-items.js';
+import SliderImages from '../../slider-images/slider-images.js';
 
-// eslint-disable-next-line prefer-arrow-callback
-Meteor.publish('siteConfig', function () {
-  return SiteConfig.find();
-});
+
+publishComposite('siteConfig', () => ({
+  find() {
+    return SiteConfig.find();
+  },
+  children: [{
+    find({ staticFrontPageTemplate }) {
+      return FrontPageTemplates.find(staticFrontPageTemplate);
+    },
+    children: [{
+      find({ sliderImages }) {
+        return SliderImages.find({ _id: { $in: sliderImages || [] } });
+      },
+    }, {
+      find({ featuredGroups }) {
+        return FeaturedGroups.find({ _id: { $in: featuredGroups || [] } });
+      },
+      children: [{
+        find({ featuredItems }) {
+          return FeaturedItems.find({ _id: { $in: featuredItems || [] } });
+        },
+      }],
+    }],
+  }],
+}));
--- app/imports/ui/containers/IndexPage.js
+++ app/imports/ui/containers/IndexPage.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { composeWithTracker } from 'react-komposer';
 import { Meteor } from 'meteor/meteor';
-import SiteConfig from '/imports/api/site-config/site-config.js';
+import SiteConfig from '../../api/site-config/site-config.js';
 import IndexPage from '../pages/IndexPage';
 
 const composer = (params, onData) => {
   const subscription = Meteor.subscribe('siteConfig');
-  
   if (subscription.ready()) {
     const siteConfig = SiteConfig.findOne();
-    console.log(siteConfig)
+    console.log(siteConfig);
     onData(null, { siteConfig, params });
   }
 };
